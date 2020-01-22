a1cce91dd5b7fd1dc1bc2bc481403d604ebeb70d
Trinity Terry <trinitycterry@gmail.com>
2020-01-22 16:04:14 -0600

    updated numbers

--- script.js
+++ script.js
@@ -394,7 +394,7 @@ businesses.forEach(business => {
 });
  */
 
-// Lightning Exercise 1: Use the reduce method on the following array to determine how much total rain fell last month.
+// Lightning Exercise 6: Use the reduce method on the following array to determine how much total rain fell last month.
 /* 
 const monthlyRainfall = [23, 13, 27, 20, 20, 31, 33, 26, 19, 12, 14, 12, 10];
 
@@ -406,7 +406,7 @@ const totalRainfall = monthlyRainfall.reduce(
 console.log(totalRainfall);
  */
 
-// Lightning Exercise 2: Use the reduce method on the following array to build a sentence.
+// Lightning Exercise 7: Use the reduce method on the following array to build a sentence.
 
 const words = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
 
