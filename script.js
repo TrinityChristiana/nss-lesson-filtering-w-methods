491f7c613d008ef63253dda33e37e0786f8944b8
Trinity Terry <trinitycterry@gmail.com>
2020-01-22 15:29:37 -0600

    end of lesson

--- script.js
+++ script.js
@@ -272,3 +272,31 @@ agents.forEach(agent => {
  */
 
 /**************************************************************************/
+// FIND METHOD
+
+document
+    .querySelector("#companySearch")
+    .addEventListener("keypress", keyPressEvent => {
+            if (keyPressEvent.charCode === 13) {
+                /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
+                const foundBusiness = businesses.find(business => {
+                     return business.companyName.includes(keyPressEvent.target.value);
+                     
+                }
+            );
+            outEl.innerHTML = `
+                <h2>
+                ${foundBusiness.companyName}
+                </h2>
+                <section>
+                ${foundBusiness.addressFullStreet}
+
+                </section>
+                <section>
+                ${foundBusiness.addressCity},
+                ${foundBusiness.addressStateCode}
+                ${foundBusiness.addressZipCode}
+                </section>
+            `;
+        }
+    });
