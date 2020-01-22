0b709284762b9c7b9ff0f0a573e150316dfd0936
Trinity Terry <trinitycterry@gmail.com>
2020-01-22 15:55:56 -0600

    finished exercise

--- script.js
+++ script.js
@@ -395,8 +395,8 @@ businesses.forEach(business => {
  */
 
 // Lightning Exercise 1: Use the reduce method on the following array to determine how much total rain fell last month.
-
-const monthlyRainfall = [23, 13, 27, 20, 20, 31, 33, 26, 19, 12, 14, 12, 10]
+/* 
+const monthlyRainfall = [23, 13, 27, 20, 20, 31, 33, 26, 19, 12, 14, 12, 10];
 
 const totalRainfall = monthlyRainfall.reduce(
     (currentValue, nextValue) => currentValue += nextValue,
@@ -404,3 +404,15 @@ const totalRainfall = monthlyRainfall.reduce(
 );
 
 console.log(totalRainfall);
+ */
+
+// Lightning Exercise 2: Use the reduce method on the following array to build a sentence.
+
+const words = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+
+const sentence = words.reduce(
+    (c, n) => c += `${n} `,
+    ""
+)
+
+console.log(sentence);
